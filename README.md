c8f739edf541a9b30f8285bbe2b3c6a331bc2ab6
phoenixyui <d0957590@o365.fcu.edu.tw>
2023-12-27 19:45:03 +0800

    add:full function

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # AutoClickChannelPoints
+到twitch->按F12->點"主控台"
+
+把autoclick.js的程式碼複製下來並貼到主控台
+接著在主控台打autoclick()
---
+++ autoclick.js
@@ -0,0 +1,5 @@
+function autoclick(){
+    while(document.getElementsByClassName("ScCoreButton-sc-ocjdkq-0 ScCoreButtonSuccess-sc-ocjdkq-5 bTXTVH fEpwrH")[0]!=null){    
+        document.getElementsByClassName("ScCoreButton-sc-ocjdkq-0 ScCoreButtonSuccess-sc-ocjdkq-5 bTXTVH fEpwrH")[0].click();
+    }
+}
